bf9bf0194b0f886d0166b803377144fa2eaa7455
serchemach <serchemach@gmail.com>
2024-03-24 22:55:23 +0300

    Add scroll to data processing

--- src/data_processing.rs
+++ src/data_processing.rs
@@ -1,6 +1,6 @@
 use std::{io::Read, vec};
 
-use egui::{include_image, CentralPanel, Color32, ComboBox, Frame, Grid, Image, Rounding, Sense, SidePanel, Stroke, Style, Ui, Vec2};
+use egui::{include_image, CentralPanel, Color32, ComboBox, Frame, Grid, Image, Rounding, ScrollArea, Sense, SidePanel, Stroke, Style, Ui, Vec2};
 use ndarray::Array1;
 use rfd::FileDialog;
 
@@ -15,7 +15,7 @@ enum ProcessingType {
 pub struct DataSet {
     raw_data: Vec<String>,
     processed_data: Option<Vec<Array1<f32>>>,
-    name: String,
+    pub name: String,
 }
 
 impl DataSet {
@@ -117,41 +117,43 @@ impl DataProcessingUI {
                 }
             });
         }
-
-        ui.vertical_centered(|ui| {
-            if self.datasets.len() == 0 {
-                ui.label("No datasets loaded");
-            }
-            else {
-                self.dataset_list(ui);
-            }
-    
-            if ui.button("Load a new dataset from file").clicked() {
-                let files = FileDialog::new()
-                    .add_filter("text", &["txt", "rs"])
-                    .add_filter("rust", &["rs", "toml"])
-                    .set_directory(".")
-                    .pick_file();
-
-                if let Some(path) = files {
-                    // ui.ctx().set_cursor_icon(egui::CursorIcon::Wait);
-                    if let Ok(mut open_file) = std::fs::File::open(&path) {
-                        let mut file_contents = String::new();
-                        let res = open_file.read_to_string(&mut file_contents);
-                        if res.is_ok() {
-                            let raw_data = file_contents.split(DATASET_SEPARATOR).map(|val| val.to_string()).collect();
-                            self.datasets.push(DataSet {
-                                raw_data,
-                                processed_data: None,
-                                name: path.file_name().unwrap().to_os_string().into_string().unwrap(),
-                            });
-                            self.shown_dataset_index = Some(self.datasets.len() - 1);
-                        }
-                    }
-                    // ui.ctx().set_cursor_icon(egui::CursorIcon::Default);
+        
+        ScrollArea::vertical().show(ui, |ui| {
+            ui.vertical_centered(|ui| {
+                if self.datasets.len() == 0 {
+                    ui.label("No datasets loaded");
                 }
+                else {
+                    self.dataset_list(ui);
+                }
+        
+                if ui.button("Load a new dataset from file").clicked() {
+                    let files = FileDialog::new()
+                        .add_filter("text", &["txt", "rs"])
+                        .add_filter("rust", &["rs", "toml"])
+                        .set_directory(".")
+                        .pick_file();
 
-            }
+                    if let Some(path) = files {
+                        // ui.ctx().set_cursor_icon(egui::CursorIcon::Wait);
+                        if let Ok(mut open_file) = std::fs::File::open(&path) {
+                            let mut file_contents = String::new();
+                            let res = open_file.read_to_string(&mut file_contents);
+                            if res.is_ok() {
+                                let raw_data = file_contents.split(DATASET_SEPARATOR).map(|val| val.to_string()).collect();
+                                self.datasets.push(DataSet {
+                                    raw_data,
+                                    processed_data: None,
+                                    name: path.file_name().unwrap().to_os_string().into_string().unwrap(),
+                                });
+                                self.shown_dataset_index = Some(self.datasets.len() - 1);
+                            }
+                        }
+                        // ui.ctx().set_cursor_icon(egui::CursorIcon::Default);
+                    }
+
+                }
+            });
         });
         
     }
